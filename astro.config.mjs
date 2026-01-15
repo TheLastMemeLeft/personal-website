74999d25f01f32d652c64889dc13e7f86d097a31
Aryan Agarwal <aryan.ag05@gmail.com>
2026-01-14 19:06:05 -0800

    yaml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thelastmemeleft.github.io', 
+  base: '/personal-website',                    
+
   vite: {
     plugins: [tailwindcss()]
   }
