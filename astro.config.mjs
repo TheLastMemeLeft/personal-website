c3818ad558748d32105d70dd255accc64d25de79
Aryan Agarwal <aryan.ag05@gmail.com>
2026-02-09 19:32:07 -0800

    pathing fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thelastmemeleft.github.io', 
-  base: '/personal-website',                    
+  base: '/',                    
 
   vite: {
     plugins: [tailwindcss()]
